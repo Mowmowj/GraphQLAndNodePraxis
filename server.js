a3d874ab22c5214ffe958d8a8ad1b1f6deb5b379
shelton cheung <274334790@qq.com>
2022-04-01 00:03:14 +0800

    feat:multify types

--- server.js
+++ server.js
@@ -5,9 +5,31 @@ var cors = require('cors')
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+#Query 是一种对象类型
+#Query 是所有查询的入口点
+#Query 必须有 不能重复
   type Query {
     hello: String
     herry: String
+    floatNum: Float
+    isSaler: Boolean
+    cardID: ID
+    user:User
+  }
+  type User {
+    name:String
+    age:Int
+    pc:Display
+    record: [String]
+    total:[Detail]
+  }
+  type Detail {
+    money:Int
+    address: String
+  }
+  type Display {
+    screen:String
+    light: Int
   }
 `);
 
@@ -18,6 +40,30 @@ var root = {
   },
   herry: ()=> {
       return `lkfjlkadflkjals`
+  },
+  floatNum: ()=>{
+      return 123213.123
+  },
+  isSaler: ()=>{
+      return false
+  },
+  cardID: ()=>{
+        return '可能是吧临安酸辣粉看啊森林等奖啊赛季分了'
+  },
+  user: ()=>{
+    return {
+        name: 'Jack',
+        age: 18,
+        pc:{
+            screen:'TFT',
+            light: 300,
+        },
+        record: ['2022','2021','2023'],
+        total :[
+            {money:30,address:'北京市'},
+            {money:30999,address:'深圳市'}
+        ]
+    }
   }
 };
 
